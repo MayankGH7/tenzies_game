29e873037b67a1b97eb798fd15cd0812c7ca65ab
MayankGH7 <108425578+MayankGH7@users.noreply.github.com>
2025-05-04 13:34:45 +0530

    fix: prevent player from toggling dice after game ends

--- src/components/Die.tsx
+++ src/components/Die.tsx
@@ -23,7 +23,7 @@ const Die = ({dieObj, holdDice, gameWon}: PropsType) => {
   } 
 
   return (
-    <button ref={btn} onClick={handleClick} className={`diebtn dark:bg-dark size-20 text-2xl p-4 rounded-xl font-bold shadow-md dark:shadow-purple-500 hover:scale-[1.1] hover:shadow-[none] transition-all duration-500 cursor-pointer  ${dieObj.isHeld && "bg-green-500 dark:bg-holdDie"}`} 
+    <button ref={btn} onClick={handleClick} disabled={gameWon} className={`diebtn dark:bg-dark size-20 text-2xl p-4 rounded-xl font-bold shadow-md dark:shadow-purple-500 hover:scale-[1.1] hover:shadow-[none] transition-all duration-500 cursor-pointer  ${dieObj.isHeld && "bg-green-500 dark:bg-holdDie"}`} 
     aria-label={`Die with value ${dieObj.value}, ${dieObj.isHeld ? "held": "not held"}`}
     aria-pressed={dieObj.isHeld}>
       {/* {dieObj.value} */}
